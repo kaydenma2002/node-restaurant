196ac3b0b315ef16690bb73633b587e4037f327b
kaydenma20023112@gmail.com <kaydenma20023112@gmail.com>
2023-08-17 15:58:22 -0700

    finish login

--- generateAccessToken.js
+++ generateAccessToken.js
@@ -1,5 +1,18 @@
 const jwt = require("jsonwebtoken")
-function generateAccessToken (user) {
-return jwt.sign(user, 'access-token', {expiresIn: "15m"})
+function generateAccessToken(user) {
+  let secretKey, expiresIn;
+  
+  if (user.user.user_type === "0") {
+      
+      expiresIn = '15m';
+  } else if (user.user.user_type === "1") {
+      
+      expiresIn = '30m';
+  } else {
+      // Handle other user types if needed
+      throw new Error("Invalid user type");
+  }
+
+  return jwt.sign(user, 'token', { expiresIn });
 }
 module.exports=generateAccessToken
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 const mysql = require("mysql");
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const generateAccessToken = require("./generateAccessToken");
 const util = require("util"); // To use promisify
@@ -15,7 +15,7 @@ const db = mysql.createPool({
   password: "password",
 
   database: "restaurant",
-  port: "3306"
+  port: "3306",
 });
 
 const connectionQuery = util.promisify(db.query).bind(db); // Promisify the query function
@@ -32,38 +32,41 @@ app.post("/api/login", async (req, res) => {
     if (searchResults.length === 0) {
       return res.sendStatus(404);
     }
-    
-    const hashedPassword = searchResults[0].password.replace('$2a$','$2y$');
-    
-    if (await bcrypt.compare(password, hashedPassword)) {
-      
-      const token = generateAccessToken({ user: email });   
-      
-      res.json({ accessToken: token });
+    const user = searchResults[0];
+    const hashedPassword = searchResults[0].password.replace("$2a$", "$2y$");
+    if (bcrypt.compare(password, hashedPassword)) {
+      const token = generateAccessToken({ user: user });
+      res.json({ user: user,token: token });
     } else {
-      
       res.status(401).send("Password incorrect");
     }
   } catch (error) {
+    console.log(error);
     res.status(500).send("Internal Server Error");
   }
 });
-app.get('/api/validate-token', (req, res) => {
+app.get("/api/validate-token", (req, res) => {
   const token = req.query.token;
-  
+
   if (!token) {
-    return res.status(400).json({ error: 'Token not provided' });
+    return res.status(400).json({ error: "Token not provided" });
   }
 
-  jwt.verify(token, 'access-token', (err, decoded) => {
+  jwt.verify(token, "token", (err, decoded) => {
     if (err) {
-      return res.status(401).json({ error: 'Invalid token' });
+      return res.status(401).json({ error: "Invalid token" });
     }
-    res.json({ valid: true });
+    
+    if (decoded.user.user_type === "0" || decoded.user.user_type === "1") {
+      console.log(decoded.user.user_type)
+      res.json({ valid: true, userType: decoded.user.user_type });
+      
+    } else {
+      res.status(401).json({ error: "Invalid user type" });
+    }
   });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
